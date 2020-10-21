19c65f0fddb91c51e46b1fc497f077754c5b2d49
mitsu1124 <m.matsumoto@startbahn.jp>
2020-10-21 23:38:20 +0900

    first commit

---
+++ config.example.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: {
+      RPC: "https://goerli.infura.io/v3/api-key",
+      POSRootChainManager: "0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74",
+      posEtherPredicate: "0xe2B01f3978c03D6DdA5aE36b2f3Ac0d66C54a6D5",
+    },
+    child: {
+      RPC: "https://rpc-mumbai.matic.today",
+      MaticWETH: "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",
+    },
+    user: {
+      privateKey:
+        "0xPvt-key",
+      address: "Public Address",
+      amount: "2000000000000000", // 0.002
+      amount2: "700000000000000000", // 0.007
+      tokenId: "1234",
+      tokenId2: "6789",
+    },
+  };
+  
---
+++ deposit.js
@@ -0,0 +1,36 @@
+const MaticPOSClient = require("@maticnetwork/maticjs").MaticPOSClient;
+const config = require("./config");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const getMaticPOSClient = () => {
+  return new MaticPOSClient({
+    network: "testnet", // optional, default is testnet
+    version: "mumbai", // optional, default is mumbai
+    parentProvider: new HDWalletProvider(
+      config.user.privateKey,
+      config.root.RPC
+    ),
+    maticProvider: new HDWalletProvider(
+      config.user.privateKey,
+      config.child.RPC
+    ),
+    posRootChainManager: config.root.POSRootChainManager,
+    parentDefaultOptions: { from: config.user.address }, // optional, can also be sent as last param while sending tx
+    maticDefaultOptions: { from: config.user.address }, // optional, can also be sent as last param while sending tx
+  });
+};
+
+const deposit = async (from, amount) => {
+  try{
+    const maticPOSClient = getMaticPOSClient();
+    const tx = await maticPOSClient.depositEtherForUser(from, amount, {
+      from,
+      gasPrice: "10000000000",
+    });
+    console.log(tx.transactionHash);
+  } catch(e){
+    console.error(e);
+  }
+};
+
+deposit(config.user.address, config.user.amount).then(() => process.exit(0));
